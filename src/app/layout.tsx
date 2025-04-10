6fa5b230060a2b51e81eb5fe00afd42eae6afbfe
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-04-10 19:19:36 -0300

    refactor: refactor layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,26 @@
 import { ThemeProvider } from '@/contexts/theme'
-import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
-import { Geist_Mono as geistMono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
-const font = geistMono({
+const inter = Inter({
   subsets: ['latin'],
+  variable: '--font-inter',
 })
 
 export const metadata: Metadata = {
   title: 'FalaAiMatheus - Portfolio',
   description: 'Created by @FalaAiMatheus',
+  creator: 'Matheus França - Fala Ai Matheus',
+  keywords: ['portfolio', 'nextjs'],
+  applicationName: 'Fala Ai Matheus Portfolio',
+  formatDetection: {
+    address: false,
+    date: false,
+    email: false,
+    telephone: false,
+    url: true,
+  },
   openGraph: {
     type: 'website',
     locale: 'pt_BR',
@@ -25,8 +35,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={cn(font.className, 'antialiased')}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <body className={`${inter.variable}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
