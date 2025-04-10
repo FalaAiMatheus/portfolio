7500e57503ae2a81db479eae32f6d334f495e995
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-04-10 18:50:39 -0300

    feat: add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { About } from '@/features/about/components/about'
+import { Contact } from '@/features/contacts/components/contact'
 import { Experiences } from '@/features/experiences/components/experiences'
 import { Projects } from '@/features/projects/components/projects-section'
 import { WelcomePage } from '@/features/welcome/components/welcome-section'
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Experiences />
       <Projects />
+      <Contact />
     </main>
   )
 }
---
+++ src/features/contacts/components/contact/index.tsx
@@ -0,0 +1,24 @@
+import { Section } from '@/layouts/section'
+import { Mail } from 'lucide-react'
+import Link from 'next/link'
+
+export const Contact = () => {
+  return (
+    <Section className="min-h-screen" id="contact">
+      <Section.Container className="flex flex-col items-start gap-3">
+        <div className="flex items-center gap-1">
+          <Mail />
+          <span>falaaimatheus.dev@gmail.com</span>
+        </div>
+        <div className="flex items-center gap-1 text-blue-600">
+          <Link
+            href="https://www.linkedin.com/in/matheussfranca/"
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+        </div>
+      </Section.Container>
+    </Section>
+  )
+}
