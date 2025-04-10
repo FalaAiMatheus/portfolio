844d1316d5d44cc74b596e36a141c215e222bc7b
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-04-10 19:10:21 -0300

    refactor: update tailwind and shadcn

--- src/features/about/components/about/index.tsx
+++ src/features/about/components/about/index.tsx
@@ -45,7 +45,7 @@ export const About = () => {
   return (
     <Section className="min-h-screen" id="about">
       <Section.Container className="flex-row justify-between gap-2 max-[925px]:flex-col-reverse">
-        <div className="flex max-w-96 w-full flex-col items-start gap-4">
+        <div className="flex w-full max-w-96 flex-col items-start gap-4">
           <h1 className="text-lg font-medium lg:text-2xl xl:text-4xl">
             Matheus França
           </h1>
@@ -71,13 +71,13 @@ export const About = () => {
           </span>
           <div className="flex w-full flex-col space-y-2 break-words">
             <p>
-              Hi there! My name is Matheus, and I'm a software developer
+              {`Hi there! My name is Matheus, and I'm a software developer`}
               passionate about technology. I have three years of experience
               working with JavaScript, React.js, TypeScript, Next.js, and
               Node.js.
             </p>
             <p>
-              I'm always learning and staying up to date with the latest trends
+              {`I'm always learning and staying up to date with the latest trends`}
               and best practices in full-stack development, exploring both
               front-end and back-end to build complete solutions.
             </p>
@@ -94,7 +94,7 @@ export const About = () => {
             <h2 className="text-lg font-medium lg:text-xl xl:text-2xl">
               Check more
             </h2>
-            <div className="flex w-full gap-2 flex-wrap">
+            <div className="flex w-full flex-wrap gap-2">
               <Button variant="outline" asChild>
                 <Link href="#experiences">Experiences</Link>
               </Button>
