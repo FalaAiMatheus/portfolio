b8c81f0a9f9dc0b2d565df8eec989c60841dbffd
FalaAiMatheus <matheussfrancads2345@gmail.com>
2026-01-27 16:10:54 -0300

    wip: configuring eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+//@ts-check
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslint from '@eslint/js';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      ...tseslint.configs.stylisticTypeChecked,
+      ...tseslint.configs.recommendedTypeChecked,
+      eslint.configs.recommended,
+    ],
+  },
+  eslintPluginPrettierRecommended,
 ]);
-
-export default eslintConfig;
