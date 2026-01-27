17feb148ce8fb17908fcb91bb0a9e45c3001edf9
FalaAiMatheus <matheussfrancads2345@gmail.com>
2026-01-27 17:00:16 -0300

    feat: adding font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
 import './globals.css';
+
+const geist = Geist({
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" className={`${geist.className} antialised`}>
       <body>{children}</body>
     </html>
   );
