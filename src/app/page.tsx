a474d502e73cef994eedd42c2f610c4d74e84795
FalaAiMatheus <matheussfrancads2345@gmail.com>
2026-01-30 11:26:48 -0300

    wip:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             animationName: 'pulse',
             animationIterationCount: 'infinite',
           }}
-        />{' '}
+        />
       </div>
     </main>
   );
